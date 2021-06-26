342f88f42e73f666089597abd059f3cc125ed5ac
Jeannie Q <jeanniequach01@gmail.com>
2021-06-26 14:04:31 -0400

    working on cover screen

--- Frontend/content.js
+++ Frontend/content.js
@@ -2,3 +2,22 @@
 chrome.runtime.onMessage.addListener(function (request) {
   alert(request);
 });
+
+//FOLLOWING CODE USED FOR PLAY AND PAUSE
+
+/* chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "toggle_video_state") {
+    var video = document.getElementsByTagName("video")[0];
+    if (video) {
+      if (video.paused) {
+        video.play();
+        sendResponse({ paused: false, tabId: request.tabId });
+      } else {
+        video.pause();
+        sendResponse({ paused: true, tabId: request.tabId });
+      }
+    } else {
+      sendResponse({ error: "No video object found" });
+    }
+  }
+}); */
