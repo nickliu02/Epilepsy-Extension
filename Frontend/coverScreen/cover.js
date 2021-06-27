48d6dba1845fa73457ae6132b18faa5e35005a90
Jeannie Q <jeanniequach01@gmail.com>
2021-06-27 02:04:30 -0400

    Revert "cleaning"
    This reverts commit f712ddd62d20b249a20a6f81afc96ed39723c0e6.

--- Frontend/coverScreen/cover.js
+++ Frontend/coverScreen/cover.js
@@ -5,7 +5,24 @@ var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
 
 var yes = document.getElementById("yes");
+// When the user clicks the button, open the modal
 
+// When the user clicks on <span> (x), close the modal
+/* span.onclick = function () {
+  window.close();
+};
+
+yes.onclick = function () {
+  window.close();
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    window.close();
+  } 
+}
+*/
 window.onload = function () {
   yes.addEventListener("click", () => {
     // Get active tab.
