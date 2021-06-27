662fcb454a84f9d12bccf30b7349567332b9c0a0
Jeannie Q <jeanniequach01@gmail.com>
2021-06-27 01:42:21 -0400

    new stling

--- Frontend/popup.js
+++ Frontend/popup.js
@@ -16,6 +16,8 @@ document.addEventListener(
     function onClickVidButton() {
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, "");
+
+        //document.getElementsByName("video")[0].onpause();
       });
     }
     function onClickLinkButton() {
@@ -26,6 +28,10 @@ document.addEventListener(
         );
       });
     }
+    /*   function onClickButton() {
+      var url = chrome.extension.getURL("./coverScreen/cover.html");
+      window.open(url);
+    } */
   },
   false
 );
@@ -44,4 +50,20 @@ window.onload = function () {
 };
 function continueScript() {
   newPopup("./coverScreen/cover.html");
+  /*   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var tab = tabs[0];
+    chrome.tabs.update(tab.id, {
+      url: chrome.extension.getURL("./coverScreen/cover.html"),
+    });
+  }); */
 }
+
+//CODE USED FOR PLAY AND PAUSE
+/* chrome.tabs.addListener(function (tabId, changeInfo, tab) {
+  var re = /www\.youtube\.com/;
+  if (re.test(tab.url) && changeInfo.title) {
+    document.getElementById("title" + tabId).textContent = changeInfo.title;
+    document.getElementById("url" + tabId).textContent = tab.url;
+  }
+});
+ */
